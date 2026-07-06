35e37a84d6004601a74df99c229d5e1a663e87d4
INF9713-NB\inf9713 <kaiser560@gmail.com>
2026-07-06 11:11:39 +0800

    111

--- get_news_titles.py
+++ get_news_titles.py
@@ -9,16 +9,17 @@ def get_news_titles(url, num_titles=20):
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
     titles = soup.select('h3 a')
-    return [title.get_text().strip() for title in titles[:num_titles]]
+    return [(title.get_text().strip(), 'https://www.chinatimes.com' + title['href']) for title in titles[:num_titles]]
 
-@app.route('/')
+@app.route('/Top20')
 def home():
     url = "https://www.chinatimes.com/realtimenews/?chdtv"
     titles = get_news_titles(url)
-    html = "<h1>前十則新聞標題</h1><ol>"
-    for title in titles:
-        html += f"<li>{title}</li>"
+    html = "<h1>中時新聞網前十則新聞標題</h1><ol>"
+    for title, href in titles:
+        html += f"<li><a href='{href}'>{title}</a></li>"
     html += "</ol>"
+    
     return html
 
 if __name__ == '__main__':
